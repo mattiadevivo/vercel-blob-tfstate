b1ae4fefb1697b6e51f55900db28205bd6d3340d
Mattia De Vivo <mattiadevivo@gmail.com>
2026-08-13 20:16:53 +0200

    add: programmatic openapi generation

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,5 +1,7 @@
 import { serve } from '@hono/node-server';
+import { swaggerUI } from '@hono/swagger-ui';
 import { Hono } from 'hono';
+import { openAPIRouteHandler } from 'hono-openapi';
 import { basicAuth } from 'hono/basic-auth';
 import { logger } from 'hono/logger';
 
@@ -7,8 +9,11 @@ import type { StateService } from '../application/state-service.js';
 import { createStateRouter } from './state/index.js';
 
 interface HttpConfig {
+    APP_VERSION: string;
     HTTP_HOST: string;
     HTTP_PORT: number;
+    HTTP_SERVER_DESCRIPTION: string;
+    HTTP_SERVER_NAME: string;
 }
 
 class HttpApp {
@@ -29,8 +34,30 @@ class HttpApp {
             }),
         );
 
-        const stateRouter = createStateRouter(stateService);
-        this.app.route('/state', stateRouter);
+        this.app.route('/state', createStateRouter(stateService));
+        this.app.get(
+            '/openapi',
+            openAPIRouteHandler(this.app, {
+                documentation: {
+                    info: {
+                        description: this.config.HTTP_SERVER_DESCRIPTION,
+                        title: this.config.HTTP_SERVER_NAME,
+                        version: this.config.APP_VERSION,
+                    },
+                    servers: [
+                        {
+                            description: 'Local Server',
+                            url: `http://${this.config.HTTP_HOST}:${this.config.HTTP_PORT}`,
+                        },
+                        {
+                            description: 'Local Server - localhost',
+                            url: `http://localhost:${this.config.HTTP_PORT}`,
+                        },
+                    ],
+                },
+            }),
+        );
+        this.app.get('/ui', swaggerUI({ url: '/openapi' }));
     }
 
     run(): void {
